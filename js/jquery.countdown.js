77238d64a397e799a9e0248521454538c8b320a8
raphael <raphaelsantosdesigner@gmail.com>
2019-12-07 17:46:44 -0800

    last commit

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -242,7 +242,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Dec 31, 2019 00:00:00').getTime(),
+let countDown = new Date('Dec 31, 2019 20:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
